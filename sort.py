5e62158e07e8579c488df228d29a443b49b96910
syan.sha <syan.sha@qunar.com>
2016-10-18 18:30:25 +0800

    added shell sort and radix sort

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import math
 from timeit import default_timer as timer
 
 def insert_sort(a):
@@ -13,9 +14,28 @@ def insert_sort(a):
         a[j+1] = key
     return a
 
+# gap = n/(2^i)
+def shell_sort(a):
+    n = len(a)
+    gap = round(n/2)
+    while gap > 0:
+        for i in range(gap, n):
+            key = a[i]
+            j = i-gap 
+            while j >= 0 and a[j] > key: 
+                a[j+gap] = a[j]
+                j -= gap  
+            a[j+gap] = key 
+            # other style
+            # j = i 
+            # while j >= gap and a[i - gap] > key:
+            #     a[j] = a[j-gap]
+            #     j -= gap
+            # a[j] = key
 
-def shell_sort():
-    pass
+        gap  = round(gap/2)
+    return a 
+
 
 
 
@@ -55,6 +75,7 @@ def merge_sort(a):
     merge_sort(left)
     merge_sort(right)
     merge(a,left,right)      
+    return a 
 
 def partition(a, p, r):
     i = p-1
@@ -72,12 +93,65 @@ def quick_sort(a, p, r):
         q = partition(a, p, r)
         quick_sort(a, p, q-1) # NOTICE q-1 not q
         quick_sort(a, q+1,r)  # NOTICE q+1 not q
+    return a 
 
-def count_sort():
-    pass
+def bit_count_sort(a, b, pss, r):
+    n = len(a)
+    mask = 1
+    # mask 为二进制数 r个1
+    for i in range(1, r):
+        mask |= (1<<i)
+    # print ('mask', mask)
+    c = [0]*(mask+1) # 每一位都有 mask+1 种选择
+    move = (pss*r)
+    # print('move', move)
+    
+    bit_v = mask<<move
+    # print ('bit_v', bit_v)
 
-def radix_sort():
-    pass
+    # print ('a', a)
+    aa = list(map(lambda x : (x&bit_v)>>move, a)) # loc bit value
+
+    # print ('aa', aa)
+    # c[i] contains number of i 
+    for i in range(0, n):
+        c[aa[i]] += 1
+
+    # print ('c', c)
+    # c[i] contains number of elements <= i
+    for i in range(1,mask+1):
+        c[i] += c[i-1]
+
+    # print ('c', c)
+
+    for j in range(n-1, -1, -1):
+        b[c[aa[j]]-1] = a[j]
+        c[aa[j]] -= 1
+    # print (b)
+
+    return b 
+
+
+def radix_sort(a, bit):
+    n = len (a)
+    tmpa = [0]*n
+    r = round(math.log2(n))
+    # print('r' , r)
+    pss = math.ceil(1.0*bit/r)
+    # print('passes', pss)
+    flag = 0
+    for i in range(0,pss):
+        if flag == 0:
+            bit_count_sort(a,tmpa, i, r)
+        else:
+            bit_count_sort(tmpa, a, i, r)
+        flag ^= 1
+
+    if flag == 1:
+        return tmpa
+    else:
+        return a 
+
 
 def random_int(max, n):
     return random.sample(range(0,max),n)
@@ -100,13 +174,16 @@ def judge_same(a, b):
     return 1
    
 def test():
-    max_v = 2**32 -1 
+    max_bit = 32
+    max_v = 2**max_bit -1 
     max_n = 10**2
     a = random_int(max_v, max_n)
     n = len(a)
     # insert_sort(a)
     # merge_sort(a)
-    quick_sort(a, 0, n-1)
+    # quick_sort(a, 0, n-1)
+    # shell_sort (a)
+    a = radix_sort(a, max_bit)
     print(a)
     print(judge(a))
 
